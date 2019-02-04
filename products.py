a03e0c85a1456cf385294b00613d4ad8baf97130
long siang <w2955307@gmail.com>
2019-02-04 18:29:01 +0800

    add file check out

--- products.py
+++ products.py
@@ -1,4 +1,20 @@
+import os #operating system
+
+#讀取檔案
 products = []
+if os.path.isfile('products.csv'): #檢查檔案再佈在
+	print('yeah! 找到檔案!')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案....')
+
+#讓使用者輸入
 while True:
 	name = input('請您輸入商品名稱: ')
 	if name == 'q':
@@ -8,9 +24,11 @@ while True:
 	products.append(p)
 print(products)
 
+#印出所有商品購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品, 價格\n')
 	for p in products:
